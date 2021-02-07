69a24652134f7940753a40a7bd71b0c15fc31524
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2021-02-07 20:48:07 +0200

    Add frontend production build

---
+++ build/precache-manifest.8416bee6584c376b97b016b1a5e6bb95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64efea5db1b60aefe22721c9bb2c5a7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cecff66644f73fc80c9",
+    "url": "/static/js/2.357f7ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.357f7ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5460db414d35a104851f",
+    "url": "/static/js/main.2123d6d7.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
