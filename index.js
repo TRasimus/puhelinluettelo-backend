a7771f0cfd099f2403914f6d191085a7a3393b65
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2024-04-04 19:51:49 +0300

    Tehtävä 3.15

--- index.js
+++ index.js
@@ -53,18 +53,26 @@ let persons =  [
    })
   })
 
-  app.get('/api/persons/:id', (request, response) => {
+  app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    Person.findById(id).then(person => {
-      response.json(person)
-    })
+    Person.findById(id)
+      .then(person => {
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end
+        } 
+      })
+      .catch(error => next(error))
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
+  app.delete('/api/persons/:id', (request, response, next) => {
     const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
 
 /*
@@ -105,6 +113,18 @@ let persons =  [
       response.json(savedPerson)
     })
   })
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+
+  app.use(errorHandler)
   
   const PORT = process.env.PORT
   app.listen(PORT, () => {
